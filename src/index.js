f88a1b5c1d753b6cac24f8f9816b25d645011bb8
Liliya <liliyavoloshine@gmail.com>
2021-07-01 11:33:49 +0400

    refactor: add arguments checking

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 exports.min = function min(array) {
-  console.log(array)
-  if (array.length > 0 && arguments != undefined) {
-    console.log(Math.max(...array) ) 
-    return Math.min(...array) 
+  if (min.arguments.length > 0) {
+    if (array.length > 0) {
+      return Math.min(...array)
+    } else {
+      return 0
+    }
   } else {
-    console.log(0)
     return 0
   }
 }
 
 exports.max = function max(array) {
-  if (array.length > 0 && arguments != undefined) {
-   console.log(Math.max(...array) ) 
-    return Math.max(...array) 
+  if (max.arguments.length > 0) {
+    if (array.length > 0) {
+      return Math.max(...array)
+    } else {
+      return 0
+    }
   } else {
-    console.log(0)
     return 0
   }
 }
 
 exports.avg = function avg(array) {
-  if (array.length > 0 && arguments != undefined) {
-    const sum = array.reduce((sum, current) => sum + current, 0)
-    let avg = sum / array.length
-    return avg
+  if (avg.arguments.length > 0) {
+    if (array.length > 0) {
+      const sum = array.reduce((sum, current) => sum + current, 0)
+      let avg = sum / array.length
+      return avg
+    } else {
+      return 0
+    }
   } else {
-    console.log(0)
     return 0
   }
 }
