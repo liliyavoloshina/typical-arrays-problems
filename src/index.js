1d307f866b5f72392feb9ffe1061a71de1713dde
Liliya <liliyavoloshine@gmail.com>
2021-07-01 10:40:49 +0400

    feat: add min/max logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array.length > 0) {
+    return Math.min(...array) 
+  } else {
+    return 0
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array.length > 0) {
+    return Math.max(...array) 
+  } else {
+    return 0
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array.length > 0) {
+    const sum = 0
+    for (n in array) {
+      sum+=n
+    }
+  } else {
+    return 0
+  }
 }
