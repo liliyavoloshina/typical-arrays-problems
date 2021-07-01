d0edeecd61fc1e5b4b13d1eff73b3fc4fd37157d
Liliya <liliyavoloshine@gmail.com>
2021-07-01 10:59:27 +0400

    feat: add avg logic

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 exports.min = function min(array) {
-  if (array.length > 0) {
+  console.log(array)
+  if (array.length > 0 && arguments != undefined) {
+    console.log(Math.max(...array) ) 
     return Math.min(...array) 
   } else {
+    console.log(0)
     return 0
   }
 }
 
 exports.max = function max(array) {
-  if (array.length > 0) {
+  if (array.length > 0 && arguments != undefined) {
+   console.log(Math.max(...array) ) 
     return Math.max(...array) 
   } else {
+    console.log(0)
     return 0
   }
 }
 
 exports.avg = function avg(array) {
-  if (array.length > 0) {
-    const sum = 0
-    for (n in array) {
-      sum+=n
-    }
+  if (array.length > 0 && arguments != undefined) {
+    const sum = array.reduce((sum, current) => sum + current, 0)
+    let avg = sum / array.length
+    return avg
   } else {
+    console.log(0)
     return 0
   }
 }
